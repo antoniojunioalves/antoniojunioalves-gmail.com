0897a2b4864ddb19dc9474d23f95c53180938fe7
Antônio Júnio Alves Ferreira <antoniojunioalves@gmail.com>
2020-03-26 17:04:12 -0300

    Inicio exercício 5

---
+++ src/components/Dados/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dados = ({ id, nome, genero, documento, telefone, email, descricaoCargo }) => {
+  return (
+    <div>Dados</div>
+  ); 
+}
+
+export default Dados;
---
+++ src/components/LotacaoPrincipal/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LotacaoPrincipal = ({ idForo, descricaoForo, nomeMunicipio }) => {
+  return (
+    <div>LotacaoPrincipal</div>
+  );
+}
+
+export default LotacaoPrincipal;
